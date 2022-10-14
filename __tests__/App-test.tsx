923ce7ca375c8149ea6079296b115e364b0b1d64
Héctor Carramiñana <hectorcarraminana@PRF303.local>
2022-10-14 20:41:24 +0200

    Added Test: PhoneCard component

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,26 @@
-/**
- * @format
- */
-
-import 'react-native';
-import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import PhoneCard from '../src/components/common/PhoneCard'
+import { INavigation } from '../src/components/common/Form/Interfaces/index';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
-});
+  renderer.create(
+    <PhoneCard
+      navigation={{ navigate: (route: string, params?: object): void => {} } as INavigation}
+      item={{
+        _id: '5f9f1b9b0b1b9c0017b8b1a1',
+        id: 1,
+        name: 'iPhone 11',
+        manufacturer: 'Apple',
+        description: 'A large phone with one of the best screens',
+        color: 'black',
+        price: 699,
+        imageFileName: 'https://fakeimages.com/IPhone_11.png',
+        screen: '6.1 inches IPS',
+        processor: 'A13 Bionic',
+        ram: 4,
+      }}
+    />
+  )
+})
